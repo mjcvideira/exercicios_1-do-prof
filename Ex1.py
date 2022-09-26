16a0f0d9993b36c0fcf46a7d1afd3d8d5141da28
Dan Medeiros <dan.medeiros@sagewell.com>
2022-09-26 11:06:18 +0000

    Exercicios

---
+++ Ex1.py
@@ -0,0 +1,10 @@
+"""
+Declare uma lista para guardar as vendas do grupo central
+Declara uma lista para guardar os nomes das cinco ilhas do grupo central
+Peça dados ao utilizador e guarde-os na lista
+Após o utilizador ter inserido os 5 valores apresente:
+- Total das vendas
+- O menor valor inserido
+- O maior valor inserido
+- A média das vendas
+"""
